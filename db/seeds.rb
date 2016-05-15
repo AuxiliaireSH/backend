7ff9853b74e4f8102f78fcb1a1468648a16528a7
Emil Edholm <bobby@verypublic.org>
2016-05-15 11:49:02 +0200

    Add the seeds used during the presentation

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ users = User.create(
     {firstname: 'Johan', lastname: 'Andersson', username: 'kruken', password:'apabepa123', email:'kruken@example.org'}
   ])
 jobs = Job.create([
-  {title: 'Wash my car', description: 'My Ferrari needs washing. ' + "lorem ipsum "*10, price: 1000, kind: 'Carwash', latitude: 57.686292, longitude:11.983471, user_id: 1, created_at: "2016-05-10T12:00:22.776Z"},
-    {title: 'Shitters clogged', description: 'Need help ASAP!' + "lorem ipsum "*10, price: 1000, kind: 'Plumbing', latitude: 57.698917, longitude:11.946467, user_id: 2, created_at: "2016-05-9T11:37:22.776Z"},
-    {title: 'Furniture assembly', description: 'My IKEA furniture needs assembly' + "lorem ipsum "*10, price: 500, kind: 'Assembly', latitude:57.692993, longitude:12.010803, user_id: 3, created_at: "2016-05-14T19:43:22.776Z"},
-    {title: 'Computer not working', description: 'The screen is black' + "lorem ipsum "*10, price: 9999, kind: 'IT', latitude:57.698864, longitude:11.99295, user_id: 4, created_at: "2016-05-13T15:13:22.776Z"}
+  {title: 'Wash my car', description: 'My Ferrari needs washing. I can\'t afford a car wash so price is low.', price: 40, kind: 'Carwash', latitude: 57.686292, longitude:11.983471, user_id: 1, created_at: "2016-05-10T12:00:22.776Z"},
+    {title: 'Shitters clogged', description: 'I did a poopsiewoopsie, need help ASAP!', price: 1000, kind: 'Plumbing', latitude: 57.698917, longitude:11.946467, user_id: 2, created_at: "2016-05-14T11:37:22.776Z"},
+    {title: 'Furniture assembly', description: 'My IKEA furniture needs assembly. Can some kind soul help me?', price: 150, kind: 'Assembly', latitude:57.692993, longitude:12.010803, user_id: 3, created_at: "2016-05-14T19:43:22.776Z"},
+    {title: 'Computer not working', description: 'The screen is black. Have NOT tried to restart it.', price: 99, kind: 'IT', latitude:57.698864, longitude:11.99295, user_id: 4, created_at: "2016-05-13T15:13:22.776Z"}
 ])
